a81316c49500997eebc6a4ce8499d189c8aba49c
ashgazer <ashik1000@hotmail.com>
2020-11-06 16:40:18 +0000

    add os exit if dest equal base

--- src/main.go
+++ src/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
+	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	var baseFlag = flag.String("b", "GBP", "your starting currency code")
-	var destFlag = flag.String("d", "EUR", "your destination currency code")
+	var destFlag = flag.String("d", "GBP", "your destination currency code")
 	var conFlag = flag.String("c", "n", "do you wish to convert money?")
 	flag.Parse()
+	
+	if *destFlag == *baseFlag {
+		os.Exit(0)
+	}
 	money, _ := strconv.ParseFloat(*conFlag, 64)
-
 
 	var baseCurrerncy, DestCurrency string
 	var baseCurrerncyP *string = &baseCurrerncy
@@ -25,7 +29,7 @@ func main() {
 	rate := getRate(baseCurrerncy, DestCurrency)
 
 	if int(money) > 0 {
-	
+
 		fmt.Printf("%.2f \n", calcChange(money, rate))
 
 	}
