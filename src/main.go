a9b99427874e74e19743157c6acc01b40ec1e331
ashgazer <ashik1000@hotmail.com>
2020-11-06 16:00:51 +0000

    change so that cli accepts command line arguments

--- src/main.go
+++ src/main.go
@@ -1,27 +1,27 @@
 package main
 
-import "fmt"
-import "strings"
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
 
 func main() {
+	var baseFlag = flag.String("b", "GBP", "your starting currency code")
+	var destFlag = flag.String("d", "EUR", "your destination currency code")
+	var conFlag = flag.String("c", "n", "do you wish to convert money?")
+	flag.Parse()
 
-	var baseCurrerncy, DestCurrency, ans string
+	var baseCurrerncy, DestCurrency string
 	var baseCurrerncyP *string = &baseCurrerncy
 	var DestCurrencyP *string = &DestCurrency
-	fmt.Println("what is the currency you want to change from?")
-	fmt.Scanln(&baseCurrerncy)
-	fmt.Println("what is the currency you want to change to?")
-	fmt.Scanln(&DestCurrency)
 
-	*DestCurrencyP = strings.ToUpper(DestCurrency)
-	*baseCurrerncyP = strings.ToUpper(baseCurrerncy)
+	*DestCurrencyP = strings.ToUpper(*destFlag)
+	*baseCurrerncyP = strings.ToUpper(*baseFlag)
 
 	rate := getRate(baseCurrerncy, DestCurrency)
 
-	fmt.Println("\n\nDo you want to convert your money? y/n")
-	fmt.Scanln(&ans)
-
-	if ans == "y" {
+	if *conFlag == "y" {
 		var money float64
 		fmt.Println("How much money do you want to change?")
 
